c624c9881317f947a2e761860d6f6791d2935b45
Lenovo-PC\Santhi GN <santhi_75@yahoo.com>
2019-05-04 15:39:20 +0530

    consoleBuddy

---
+++ GridWebServices/BuddyProcessingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Core.Enums;
+using Core.Extensions;
+using Core.Helpers;
+using InfrastructureService;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace BuddyProcessingApp
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+        }
+
+     
+    }
+}
