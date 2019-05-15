a87b5ab58548d9700caf662c87e368912ffe59e7
Lenovo-PC\Santhi GN <santhi.gn@pintlab.com>
2019-05-15 20:05:07 +0530

    Account Invoice fix

--- GridWebServices/OrderService/Controllers/OrdersController.cs
+++ GridWebServices/OrderService/Controllers/OrdersController.cs
@@ -5378,7 +5378,7 @@ namespace OrderService.Controllers
                                 FinalAmount = accountInvoiceRequest.FinalAmount,
                                 InvoiceUrl = downloadLinkPrefix + accountInvoiceRequest.InvoiceID,
                                 Remarks = Misc.Account.ToString(),
-                                PaymentSourceID = int.Parse(((BSSAccount)accountResponse.Results).AccountNumber),
+                                PaymentSourceID = ((BSSAccount)accountResponse.Results).AccountNumber,
                                 OrderStatus = 0
 
                             };
