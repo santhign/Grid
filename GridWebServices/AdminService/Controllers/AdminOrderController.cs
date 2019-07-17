92e716307c184b923e9e6e00e31c5ee571502f5f
Preeti <preeti.g@stratagile.com>
2019-07-17 13:11:27 +0800

    split of change log

--- GridWebServices/AdminService/Controllers/AdminOrderController.cs
+++ GridWebServices/AdminService/Controllers/AdminOrderController.cs
@@ -501,10 +501,15 @@ namespace AdminService.Controllers
                             //Sending message start
                             // Send email to customer email                            ConfigDataAccess _configAccess = new ConfigDataAccess(_iconfiguration);
                             DatabaseResponse registrationResponse = await _adminOrderDataAccess.GetEmailNotificationTemplate(emailDetails.VerificationStatus == 2 ? NotificationEvent.ICValidationReject.GetDescription() : NotificationEvent.ICValidationChange.GetDescription());
-
+                            string[] changelog = emailDetails.ChangeLog.Split(";");
+                            string finallog = "";
+                            foreach (string log in changelog)
+                            {
+                                finallog = finallog + "&bull;" + log + "<br/>";
+                            }
                             var notificationMessage = MessageHelper.GetMessage(emailDetails.Email, emailDetails.Name, emailDetails.VerificationStatus == 2 ? NotificationEvent.ICValidationReject.GetDescription() : NotificationEvent.ICValidationChange.GetDescription(),
                            ((EmailTemplate)registrationResponse.Results).TemplateName,
-                       _iconfiguration, string.IsNullOrWhiteSpace(finalURL) ? "-" : finalURL, string.IsNullOrWhiteSpace(emailDetails.Remark) ? "-" : emailDetails.Remark.Replace(";", "<br />"),  string.IsNullOrWhiteSpace(emailDetails.ChangeLog) ? "-" : emailDetails.ChangeLog.Replace(";", "<br />"));
+                       _iconfiguration, string.IsNullOrWhiteSpace(finalURL) ? "-" : finalURL, string.IsNullOrWhiteSpace(emailDetails.Remark) ? "-" : emailDetails.Remark.Replace(";", "<br />"),  string.IsNullOrWhiteSpace(emailDetails.ChangeLog) ? "-" : finallog);
                             var notificationResponse = await _adminOrderDataAccess.GetConfiguration(ConfiType.Notification.ToString());
 
 
